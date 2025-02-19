834037cd0754d2542ecb4476ba269c48572c2dc8
Tyloth <miguelbernatskyy3@gmail.com>
2025-02-19 13:09:45 +0000

    LOD draw bugfix

--- bin/main.cpp
+++ bin/main.cpp
@@ -5,76 +5,74 @@
 #include "half.cpp"
 #include "half.h"
 
-void __cdecl log(int a, int b, char* msg)
-{
-	fprintf(stderr, "Log (%d %d) %s\n", a, b, msg);
-}
+void __cdecl log(int a, int b, char* msg) { fprintf(stderr, "Log (%d %d) %s\n", a, b, msg); }
 
 GrannyLogger logger = { &log, &log };
 
 int getTypeSize(int type) {
-	switch(type) {
-		case 10: return 4;  // 32bit float
-		case 11:            // granny_int8
-		case 12:            // granny_uint8
-		case 14: return 1;  // granny_uint8
-		case 21: return 2;  // 16bit float
-		default: return 0;
-	}
+    switch (type) {
+    case 10:
+        return 4; // 32bit float
+    case 11: // granny_int8
+    case 12: // granny_uint8
+    case 14:
+        return 1; // granny_uint8
+    case 21:
+        return 2; // 16bit float
+    default:
+        return 0;
+    }
 }
 
 typedef float (*convertFunc)(uint8_t* value);
 
-float convertDummy(uint8_t* value) {
-	return 0.0f;
-}
+float convertDummy(uint8_t* value) { return 0.0f; }
 
-float convert10(uint8_t* value) {
-	return *(float*)value;
-}
+float convert10(uint8_t* value) { return *(float*)value; }
 
-float convert11(uint8_t* value) {
-	return *(granny_int8*)value;
-}
+float convert11(uint8_t* value) { return *(granny_int8*)value; }
 
-float convert12or14(uint8_t* value) {
-	return *(granny_uint8*)value;
-}
+float convert12or14(uint8_t* value) { return *(granny_uint8*)value; }
 
 float convert21(uint8_t* value) {
-	uint32_t result = half_to_float(*(uint16_t*)value);
-	return *(float*)&result;
+    uint32_t result = half_to_float(*(uint16_t*)value);
+    return *(float*)&result;
 }
 
 convertFunc getConvertFunc(int type) {
-	switch (type) {
-		case 10: return &convert10;
-		case 11: return &convert11;
-		case 12:
-		case 14: return &convert12or14;
-		case 21: return &convert21;
-		default: return &convertDummy;
-	}
+    switch (type) {
+    case 10:
+        return &convert10;
+    case 11:
+        return &convert11;
+    case 12:
+    case 14:
+        return &convert12or14;
+    case 21:
+        return &convert21;
+    default:
+        return &convertDummy;
+    }
 }
 
 struct VertexComponent {
-	const char* name;     // name of the component as used in granny files
-	int32_t index;        // index of -1 means it doesn't exist
-	bool exists;          // does this component exit
-	convertFunc convert;  // convertion function to convert from this components type to 32bit float
-	uint32_t offset;      // offset relative to the start of the vertex at which this component starts
-	uint32_t typeSize;    // size in bytes of the type of this component
+    const char* name; // name of the component as used in granny files
+    int32_t index; // index of -1 means it doesn't exist
+    bool exists; // does this component exit
+    convertFunc convert; // convertion function to convert from this components type to 32bit float
+    uint32_t offset; // offset relative to the start of the vertex at which this component starts
+    uint32_t typeSize; // size in bytes of the type of this component
 };
 
 uint32_t nVertexComponents_ = 6;
 
 VertexComponent vertexComponents_[] = {
-	{ "Position"           , -1, false, &convertDummy, 0, 0 },
-	{ "Normal"             , -1, false, &convertDummy, 0, 0 },
-	{ "Tangent"            , -1, false, &convertDummy, 0, 0 },
-	{ "Binormal"           , -1, false, &convertDummy, 0, 0 },
-	{ "TextureCoordinates0", -1, false, &convertDummy, 0, 0 },
-	{ "TextureCoordinates1", -1, false, &convertDummy, 0, 0 },
+    { "Position", -1, false, &convertDummy, 0, 0 },
+    { "Normal", -1, false, &convertDummy, 0, 0 },
+    { "Tangent", -1, false, &convertDummy, 0, 0 },
+    { "Binormal", -1, false, &convertDummy, 0, 0 },
+    { "TextureCoordinates0", -1, false, &convertDummy, 0, 0 },
+    { "TextureCoordinates1", -1, false, &convertDummy, 0, 0 },
 };
 
 int nTriangleGroups_;
@@ -89,189 +87,195 @@ int nIndices_;
 uint8_t* indices_;
 
 void getMeshInfo(const Mesh_t* mesh) {
-	// get amount of TriangleGroups (Surfaces)
-	nTriangleGroups_ = _GrannyGetMeshTriangleGroupCount(mesh);
-	triangleGroups_ = _GrannyGetMeshTriangleGroups(mesh);
+    // get amount of TriangleGroups (Surfaces)
+    nTriangleGroups_ = _GrannyGetMeshTriangleGroupCount(mesh);
+    triangleGroups_ = _GrannyGetMeshTriangleGroups(mesh);
 
-	StringMember_t* sm = mesh->PrimaryVertexData->Vertices_strings;
+    StringMember_t* sm = mesh->PrimaryVertexData->Vertices_strings;
 
-	uint32_t currentOffset = 0;
+    uint32_t currentOffset = 0;
 
-	// get amount of vertex components
-	// _GrannyGetVertexComponentCount(sm); doesnt work as TextureCoordinates0
-	// and TextureCoordinates1 is counted as one component
-	// uint32_t nVertexComponents = _GrannyGetVertexComponentCount(sm);
-	uint32_t nVertexComponents = 0;
+    // get amount of vertex components
+    // _GrannyGetVertexComponentCount(sm); doesnt work as TextureCoordinates0
+    // and TextureCoordinates1 is counted as one component
+    // uint32_t nVertexComponents = _GrannyGetVertexComponentCount(sm);
+    uint32_t nVertexComponents = 0;
 
-	for(int i = 0; sm[i].Type; ++i)
-		++nVertexComponents;
+    for (int i = 0; sm[i].Type; ++i)
+        ++nVertexComponents;
 
-	// offset for each component in the vertex structure
-	uint32_t* offsets = new uint32_t[nVertexComponents];
+    // offset for each component in the vertex structure
+    uint32_t* offsets = new uint32_t[nVertexComponents];
 
-	// calculate offset for each component
-	for(uint32_t iVertexComponent = 0; iVertexComponent < nVertexComponents; ++iVertexComponent) {
-		offsets[iVertexComponent] =  currentOffset;
-		currentOffset += getTypeSize(sm[iVertexComponent].Type) * sm[iVertexComponent].Count;
-	}
+    // calculate offset for each component
+    for (uint32_t iVertexComponent = 0; iVertexComponent < nVertexComponents; ++iVertexComponent) {
+        offsets[iVertexComponent] = currentOffset;
+        currentOffset += getTypeSize(sm[iVertexComponent].Type) * sm[iVertexComponent].Count;
+    }
 
-	bytesPerVertex_ = currentOffset;
+    bytesPerVertex_ = currentOffset;
 
-	// set data for each component
-	for(uint32_t i = 0; i < nVertexComponents_; ++i) {
-		vertexComponents_[i].index    = _GrannyGetVertexComponentIndex(vertexComponents_[i].name, sm);
-		vertexComponents_[i].exists   = vertexComponents_[i].index != -1;
-		vertexComponents_[i].convert  = (vertexComponents_[i].exists) ? getConvertFunc(sm[vertexComponents_[i].index].Type) : &convertDummy;
-		vertexComponents_[i].offset   = (vertexComponents_[i].exists) ? offsets[vertexComponents_[i].index] : 0;
-		vertexComponents_[i].typeSize = (vertexComponents_[i].exists) ? getTypeSize(sm[vertexComponents_[i].index].Type) : 0;
-	}
+    // set data for each component
+    for (uint32_t i = 0; i < nVertexComponents_; ++i) {
+        vertexComponents_[i].index = _GrannyGetVertexComponentIndex(vertexComponents_[i].name, sm);
+        vertexComponents_[i].exists = vertexComponents_[i].index != -1;
+        vertexComponents_[i].convert = (vertexComponents_[i].exists)
+            ? getConvertFunc(sm[vertexComponents_[i].index].Type)
+            : &convertDummy;
+        vertexComponents_[i].offset
+            = (vertexComponents_[i].exists) ? offsets[vertexComponents_[i].index] : 0;
+        vertexComponents_[i].typeSize
+            = (vertexComponents_[i].exists) ? getTypeSize(sm[vertexComponents_[i].index].Type) : 0;
+    }
 
-	delete[] offsets;
+    delete[] offsets;
 
-	nVertices_ = _GrannyGetMeshVertexCount(mesh);
-	vertices_ = (uint8_t*)_GrannyGetMeshVertices(mesh);
+    nVertices_ = _GrannyGetMeshVertexCount(mesh);
+    vertices_ = (uint8_t*)_GrannyGetMeshVertices(mesh);
 
-	bytesPerIndex_ = _GrannyGetMeshBytesPerIndex(mesh);
-	nIndices_ = _GrannyGetMeshIndexCount(mesh);
-	indices_ = (uint8_t*)_GrannyGetMeshIndices(mesh);
-
+    bytesPerIndex_ = _GrannyGetMeshBytesPerIndex(mesh);
+    nIndices_ = _GrannyGetMeshIndexCount(mesh);
+    indices_ = (uint8_t*)_GrannyGetMeshIndices(mesh);
 }
 
-void convertMeshToObj(const Mesh_t* mesh, FILE* file) {
-
+void convertMeshToObj(const Mesh_t* mesh, FILE* file, int* nMeshOffset) {
     fprintf(stderr, "# VertexCount: %d, BytesPerVertex: %d\n", nVertices_, bytesPerVertex_);
-	fprintf(stderr, "# IndexCount: %d, BytesPerIndex: %d\n\n", nIndices_, bytesPerIndex_);
+    fprintf(stderr, "# IndexCount: %d, BytesPerIndex: %d\n\n", nIndices_, bytesPerIndex_);
 
     fprintf(file, "# Converted by evegr2toobj.exe\n");
     fprintf(file, "# VertexCount: %d\n", nVertices_);
     fprintf(file, "# IndexCount: %d\n", nIndices_);
-	fprintf(file, "\no %s\n", mesh->Name);
+    fprintf(file, "\no %s\n", mesh->Name);
 
     printf("Vertex types found:\n");
-	if(vertexComponents_[0].exists) {
-		printf("-- Position\n");
-		for(int iVertex = 0; iVertex < nVertices_; ++iVertex) {
-			uint8_t* vertex = vertices_ + bytesPerVertex_ * iVertex + vertexComponents_[0].offset;
-			fprintf(file, "v %f %f %f\n",
-			vertexComponents_[0].convert(vertex + 0 * vertexComponents_[0].typeSize),
-			vertexComponents_[0].convert(vertex + 1 * vertexComponents_[0].typeSize),
-			vertexComponents_[0].convert(vertex + 2 * vertexComponents_[0].typeSize)
-			);
-		}
-	}
+    if (vertexComponents_[0].exists) {
+        printf("-- Position\n");
+        for (int iVertex = 0; iVertex < nVertices_; ++iVertex) {
+            uint8_t* vertex = vertices_ + bytesPerVertex_ * iVertex + vertexComponents_[0].offset;
+            fprintf(file, "v %f %f %f\n",
+                vertexComponents_[0].convert(vertex + 0 * vertexComponents_[0].typeSize),
+                vertexComponents_[0].convert(vertex + 1 * vertexComponents_[0].typeSize),
+                vertexComponents_[0].convert(vertex + 2 * vertexComponents_[0].typeSize));
+        }
+    }
 
-	if(vertexComponents_[1].exists) {
-	    printf("-- Normal\n");
-		for(int iVertex = 0; iVertex < nVertices_; ++iVertex) {
-			uint8_t* vertex = vertices_ + bytesPerVertex_ * iVertex + vertexComponents_[1].offset;
-			fprintf(file, "vn %f %f %f\n",
-			vertexComponents_[1].convert(vertex + 0 * vertexComponents_[1].typeSize) / 255,
-			vertexComponents_[1].convert(vertex + 1 * vertexComponents_[1].typeSize) / 255,
-			vertexComponents_[1].convert(vertex + 2 * vertexComponents_[1].typeSize) / 255
-			);
-		}
-	}
+    if (vertexComponents_[1].exists) {
+        printf("-- Normal\n");
+        for (int iVertex = 0; iVertex < nVertices_; ++iVertex) {
+            uint8_t* vertex = vertices_ + bytesPerVertex_ * iVertex + vertexComponents_[1].offset;
+            fprintf(file, "vn %f %f %f\n",
+                vertexComponents_[1].convert(vertex + 0 * vertexComponents_[1].typeSize) / 255,
+                vertexComponents_[1].convert(vertex + 1 * vertexComponents_[1].typeSize) / 255,
+                vertexComponents_[1].convert(vertex + 2 * vertexComponents_[1].typeSize) / 255);
+        }
+    }
 
-	if(vertexComponents_[4].exists) {
-		printf("-- TexCoord0\n");
-		for(int iVertex = 0; iVertex < nVertices_; ++iVertex) {
-			uint8_t* vertex = vertices_ + bytesPerVertex_ * iVertex + vertexComponents_[4].offset;
-			fprintf(file, "vt %f %f\n",
-			vertexComponents_[4].convert(vertex + 0 * vertexComponents_[4].typeSize),
-			vertexComponents_[4].convert(vertex + 1 * vertexComponents_[4].typeSize)
-			);
-		}
-	}
+    if (vertexComponents_[4].exists) {
+        printf("-- TexCoord0\n");
+        for (int iVertex = 0; iVertex < nVertices_; ++iVertex) {
+            uint8_t* vertex = vertices_ + bytesPerVertex_ * iVertex + vertexComponents_[4].offset;
+            fprintf(file, "vt %f %f\n",
+                vertexComponents_[4].convert(vertex + 0 * vertexComponents_[4].typeSize),
+                vertexComponents_[4].convert(vertex + 1 * vertexComponents_[4].typeSize));
+        }
+    }
 
-	if(vertexComponents_[5].exists) { printf("-- TexCoord1 (unsupported)\n"); }
-    if(vertexComponents_[2].exists) { printf("-- Tangent   (unsupported)\n");   }
-    if(vertexComponents_[3].exists) { printf("-- Binormal  (unsupported)\n");  }
+    // smooth shading
+    fprintf(file, "\ns 1\n");
 
-	granny_uint32 index0;
-	granny_uint32 index1;
-	granny_uint32 index2;
-	granny_uint32 index3;
+    if (vertexComponents_[5].exists) {
+        printf("-- TexCoord1 (unsupported)\n");
+    }
+    if (vertexComponents_[2].exists) {
+        printf("-- Tangent   (unsupported)\n");
+    }
+    if (vertexComponents_[3].exists) {
+        printf("-- Binormal  (unsupported)\n");
+    }
 
-	if(bytesPerIndex_ == 2) {
-        uint16_t* indices = (uint16_t*) indices_;
-        for(int iGroup = 0; iGroup < nTriangleGroups_; ++iGroup) { // for each surface
-            fprintf(file, "g %s\n", mesh->MaterialBindings[triangleGroups_[iGroup].MaterialIndex].Material->Name);
-            for(int iTriangle = 0; iTriangle < triangleGroups_[iGroup].TriCount; ++iTriangle) {
+    granny_uint32 index0;
+    granny_uint32 index1;
+    granny_uint32 index2;
+    granny_uint32 index3;
+
+    if (bytesPerIndex_ == 2) {
+        uint16_t* indices = (uint16_t*)indices_;
+        for (int iGroup = 0; iGroup < nTriangleGroups_; ++iGroup) { // for each surface
+            fprintf(file, "g %s\n",
+                mesh->MaterialBindings[triangleGroups_[iGroup].MaterialIndex].Material->Name);
+            for (int iTriangle = 0; iTriangle < triangleGroups_[iGroup].TriCount; ++iTriangle) {
                 index0 = (triangleGroups_[iGroup].TriFirst + iTriangle) * 3;
-                index1 = indices[index0 + 0] + 1;
-                index2 = indices[index0 + 1] + 1;
-                index3 = indices[index0 + 2] + 1;
-                fprintf(file, "f %d/%d/%d %d/%d/%d %d/%d/%d\n",
-                index1, index1, index1,
-                index2, index2, index2,
-                index3, index3, index3);
+                index1 = indices[index0] + *nMeshOffset;
+                index2 = indices[index0 + 1] + *nMeshOffset;
+                index3 = indices[index0 + 2] + *nMeshOffset;
+                fprintf(file, "f %d/%d/%d %d/%d/%d %d/%d/%d\n", index1, index1, index1, index2,
+                    index2, index2, index3, index3, index3);
+            }
+        }
+    } else if (bytesPerIndex_ == 4) {
+        uint32_t* indices = (uint32_t*)indices_;
+        for (int iGroup = 0; iGroup < nTriangleGroups_; ++iGroup) { // for each surface
+            fprintf(file, "g %s\n",
+                mesh->MaterialBindings[triangleGroups_[iGroup].MaterialIndex].Material->Name);
+            for (int iTriangle = 0; iTriangle < triangleGroups_[iGroup].TriCount; ++iTriangle) {
+                index0 = (triangleGroups_[iGroup].TriFirst + iTriangle) * 3;
+                index1 = indices[index0] + *nMeshOffset;
+                index2 = indices[index0 + 1] + *nMeshOffset;
+                index3 = indices[index0 + 2] + *nMeshOffset;
+                fprintf(file, "f %d/%d/%d %d/%d/%d %d/%d/%d\n", index1, index1, index1, index2,
+                    index2, index2, index3, index3, index3);
             }
         }
     }
-    else if(bytesPerIndex_ == 4) {
-        uint32_t* indices = (uint32_t*) indices_;
-        for(int iGroup = 0; iGroup < nTriangleGroups_; ++iGroup) { // for each surface
-            fprintf(file, "g %s\n", mesh->MaterialBindings[triangleGroups_[iGroup].MaterialIndex].Material->Name);
-            for(int iTriangle = 0; iTriangle < triangleGroups_[iGroup].TriCount; ++iTriangle) {
-                index0 = (triangleGroups_[iGroup].TriFirst + iTriangle) * 3;
-                index1 = indices[index0 + 0] + 1;
-                index2 = indices[index0 + 1] + 1;
-                index3 = indices[index0 + 2] + 1;
-                fprintf(file, "f %d/%d/%d %d/%d/%d %d/%d/%d\n",
-                index1, index1, index1,
-                index2, index2, index2,
-                index3, index3, index3);
-            }
-        }
-    }
+    *nMeshOffset += nVertices_;
 }
 
 int main(int argc, char** argv) {
+    if (argc < 3) {
+        fprintf(stdout, "Usage: %s <src> <dst>\n", argv[0]);
+        exit(1);
+    }
 
-	if(argc < 3) {
-		fprintf(stdout, "Usage: %s <src> <dst>\n", argv[0]);
-		exit(1);
-	}
+    granny_int32 result;
 
-	granny_int32 result;
+    fprintf(stderr, "\nGranny Version: %s\n\n", _GrannyGetVersionString());
+    result = _GrannySetLogCallback(&logger);
 
-	fprintf(stderr, "\nGranny Version: %s\n\n", _GrannyGetVersionString());
-	result = _GrannySetLogCallback(&logger);
+    GrannyFile_t* grannyFile = _GrannyReadEntireFile(argv[1]);
 
-	GrannyFile_t* grannyFile = _GrannyReadEntireFile(argv[1]);
+    if (!grannyFile) {
+        printf("Could not load %s\n", argv[1]);
+        exit(1);
+    }
 
-	if(!grannyFile) {
-		printf("Could not load %s\n", argv[1]);
-		exit(1);
-	}
+    FileInfo_t* fileInfo = _GrannyGetFileInfo(grannyFile);
 
-	FileInfo_t* fileInfo = _GrannyGetFileInfo(grannyFile);
+    if (!fileInfo) {
+        printf("Could not get fileinfo\n");
+        exit(1);
+    }
 
-	if(!fileInfo) {
-		printf("Could not get fileinfo\n");
-		exit(1);
-	}
-
-    if(!fileInfo->nMeshes) {
+    if (!fileInfo->nMeshes) {
         printf("Could not find any meshes in %s\n", argv[1]);
         exit(1);
     }
 
-	granny_int32 nModels = fileInfo->nModels;
-	granny_int32 nMeshes = fileInfo->nMeshes;
+    granny_int32 nModels = fileInfo->nModels;
+    granny_int32 nMeshes = fileInfo->nMeshes;
+    granny_int32 nMeshOffset = 1;
 
-	FILE* file = fopen(argv[2], "w");
+    FILE* file = fopen(argv[2], "w");
 
-	for(int iMesh = 0; iMesh < nMeshes; ++iMesh) {
-		Mesh_t* mesh = fileInfo->Meshes[iMesh];
-		char* name = mesh->Name;
-		printf("Mesh: %s (%d)\n", name, iMesh);
-		getMeshInfo(mesh);
-		convertMeshToObj(mesh, file);
-		printf("\n");
-	}
-	fclose(file);
+    for (int iMesh = 0; iMesh < nMeshes; ++iMesh) {
+        Mesh_t* mesh = fileInfo->Meshes[iMesh];
+        char* name = mesh->Name;
+        printf("Mesh: %s (%d)\n", name, iMesh);
+        getMeshInfo(mesh);
+        convertMeshToObj(mesh, file, &nMeshOffset);
+        printf("\n");
+    }
+    fclose(file);
 
-	_GrannyFreeFile(grannyFile);
-	return 0;
+    _GrannyFreeFile(grannyFile);
+    return 0;
 }
